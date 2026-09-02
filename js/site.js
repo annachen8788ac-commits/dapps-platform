8313a0264b213ec880a513f184dcb6dc971c6de4
annachen8788ac-commits <annachen.8788ac@gmail.com>
2026-09-02 10:14:47 +0800

    Bust cache for guaranteed mobile vertical scrolling

--- js/site.js
+++ js/site.js
@@ -131,10 +131,10 @@ if(!reduceMotion&&'IntersectionObserver' in window){
  if(normalizePath(location.pathname)!=='/markets/')return;
  const css=document.createElement('link');
  css.rel='stylesheet';
- css.href='/css/market-mobile.css?v=20260902-mobile6';
+ css.href='/css/market-mobile.css?v=20260902-mobile7';
  document.head.appendChild(css);
  const js=document.createElement('script');
- js.src='/js/market-mobile-chart.js?v=20260902-mobile6';
+ js.src='/js/market-mobile-chart.js?v=20260902-mobile7';
  js.defer=true;
  document.body.appendChild(js);
 })();
